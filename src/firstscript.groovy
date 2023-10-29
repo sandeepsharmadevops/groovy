64f7b76c22849f3d4de9e0d2863f6b6d9e522ab2
SandeepSharma <sandeeepsharma9@gmail.com>
2023-10-29 20:21:58 +0530

    test

--- src/firstscript.groovy
+++ src/firstscript.groovy
@@ -1,2 +1,4 @@
-def gitVariable = "this variable is from git"
-println "this groovy file is downloaded from git!!!dqwdqwdwqdwq"
+def myfunc() {
+  def gitVariable = "this variable is from git"
+  println "this groovy file is downloaded from git!!!dqwdqwdwqdwq"
+}
